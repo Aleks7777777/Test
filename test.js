8aab5e5ad9477dbe0ec2e9a0c152ff87b760de6f
Aleks <777777ot@gmail.com>
2022-12-10 17:17:18 +0200

    777

---
+++ test.js
@@ -0,0 +1,35 @@
+console.log("test");
+
+function includes(array, value) {
+	// Change code below this line
+	let numb = false;
+	for (let i of array) {
+		if (i === value) {
+			numb = true;
+		}
+	}
+	return numb;
+	// Change code above this line
+}
+
+
+
+
+
+
+Aleks(me) = new
+	Aleks();
+while (me.Alive) {
+	me.WakeUp();
+	me.Eat();
+	me.Sport();
+	me.DoCode();
+	me.Sleep();
+}
+
+
+
+
+
+
+
